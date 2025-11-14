4e4b79ba738909010502b04020860e72da289f8d
Ugochinyere Vivian <your_github_email@example.com>
2025-11-14 10:21:29 +0100

    i added a new folder for spicy snacks

---
+++ leap_year.py
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+    public class leapyear{
+public static void main(String[]arags){
+
+Scanner input = new Scanner(System.in);
+
+System.out.println("enter the number of days in a year\n");
+int days = nextInt();
+
+if (days % 4 == 0 &&  days % 100){
+        System.out.println("This a leap year");
+}else {
+        System.out.println("this is not leap year");   
+} 
+
+ }
